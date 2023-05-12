5a8d0d81eb9916236a360b572889cd1f0ad418a5
Replit user <>
2023-05-12 01:12:08 +0000

    new try

--- heap.c
+++ heap.c
@@ -50,8 +50,8 @@ void heap_pop(Heap* pq){
   pq->heapArray[0]=pq->heapArray[pq->size-1];
   pq->size--;
   for(int i=0; i< pq->size; i++){ 
-    int son=2*i+1;
-    int son2=2*i+2;
+    int son=2i+1;
+    int son2=2i+2;
     int taita=(i-1)/2;
     if(pq->heapArray[son].priority > pq->heapArray[son2].priority){
       heapElem aux=pq->heapArray[taita];
