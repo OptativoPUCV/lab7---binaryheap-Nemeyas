aa5531aef5970308ef6df99905f24f1251c450be
Replit user <>
2023-05-12 00:25:51 +0000

    new try

--- heap.c
+++ heap.c
@@ -28,7 +28,7 @@ void heap_push(Heap* pq, void* data, int priority){
   int i=pq->size;
   while(i > 0 && pq->heapArray[i].priority > pq->heapArray[(i-1)/2].priority){ //ya no
     int aux=pq->heapArray[i].priority;
-    pq->heapArray[i].priority = pq->heapArray[i]priority;
+    pq->heapArray[i].priority = pq->heapArray[i].priority;
     pq->heapArray[i].priority = aux;
   }
 }
