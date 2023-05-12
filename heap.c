c6626ce358160dd22edd2824cc3e40e5d741617d
Replit user <>
2023-05-12 00:08:40 +0000

    new try

--- heap.c
+++ heap.c
@@ -36,7 +36,7 @@ Heap* createHeap(){
   Heap* mon = (Heap*) malloc(sizeof(Heap));
   mon->capac=3;
   mon->size=0;
-  mon->heapArray= (heapElem*) realloc(mon->heapArray, sizeof(heapElem)*mon->capac);
+  mon->heapArray= (heapElem*) realloc(mon, sizeof(heapElem)*mon->capac);
 
    return mon;
 }
